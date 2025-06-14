f986ee9962c7779f020029c95967f6ccdd96a089
kadelcode <kadelcode@gmail.com>
2025-06-14 13:50:45 +0100

    Update code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 👇 This tells Nest to serve files from the public/ folder
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  await app.listen(3000);
+  console.log('Server running on http://localhost:3000/qr.html');
 }
 bootstrap();
