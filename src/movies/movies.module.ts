28fc4ba3c08c58ce25a9389d4911daf581a45afe
kadelcode <kadelcode@gmail.com>
2025-06-14 20:08:09 +0100

    Update & document code

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,11 +1,17 @@
 // movies.module.ts
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
-import { QrService } from '../qr/qr.service';
+// import { QrService } from '../qr/qr.service'; // Import the shared service
+import { QrModule } from 'src/qr/qr.module';
 
+// '@Module' decorator used to define a NestJs module
 @Module({
-  controllers: [MoviesController],
-  providers: [QrService],
-  exports: [],
+  controllers: [MoviesController], // Controllers that belong to this module
+  providers: [], // Providers that will be instantiated by the NestJS injector
+  exports: [], // This array lists the subset of providers that should be available to other modules
+  imports: [QrModule] // This array lists the modules that are required by this module
 })
+
+// The 'MoviesModule' class is defined and exported
+// This class serves as the module configuration container.
 export class MoviesModule {}
